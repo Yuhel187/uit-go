977e5752c81bf85f592db77ec30caacfbc5a3d31
Yuhel187 <23520610@gm.uit.edu.vn>
2025-11-02 21:11:37 +0700

    add simple terraform code

---
+++ infra/main.tf
@@ -0,0 +1,69 @@
+# ----------------------
+# VPC
+# ----------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "rt_assoc" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id      = aws_subnet.public.id
+}
+
+# ----------------------
+# Security Group
+# ----------------------
+resource "aws_security_group" "rds_sg" {
+  name   = "rds-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ----------------------
+# RDS
+# ----------------------
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "15"
+  instance_class       = "db.t3.micro"
+  username             = var.db_username
+  password             = var.db_password
+  db_name              = var.db_name
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "db_username" {
+  default = "admin"
+}
+
+variable "db_password" {
+  default = "SuperSecret123!"
+}
+
+variable "db_name" {
+  default = "userdb"
+}
