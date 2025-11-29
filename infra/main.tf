f83c22c6458e51ec5e8c70a63f283e3177e1e7f0
Nguyen Quang Khai <khainq205@gmail.com>
2025-11-30 02:50:36 +0700

    feat: merge performanceTesting branch

--- infra/main.tf
+++ infra/main.tf
@@ -406,7 +406,7 @@ resource "aws_lb_listener_rule" "driver_rule" {
   
   condition { 
     path_pattern { 
-      values = ["/drivers/*"] 
+      values = ["/drivers", "/drivers/*"] 
     } 
   }
 }
